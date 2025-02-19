7e9e67e36423c689b25f5ca26447993a059190aa
alievaleila <meliyeva452@gmail.com>
2025-02-20 00:57:04 +0400

    initial commit

---
+++ src/main/java/org/example/easy/javaabstractclass/Book.java
@@ -0,0 +1,11 @@
+package org.example.easy.javaabstractclass;
+
+abstract class Book {
+    String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    abstract void setTitle(String s);
+}
---
+++ src/main/java/org/example/easy/javaabstractclass/MyBook.java
@@ -0,0 +1,8 @@
+package org.example.easy.javaabstractclass;
+
+public class MyBook extends Book{
+    @Override
+    void setTitle(String s) {
+        title=s;
+    }
+}
